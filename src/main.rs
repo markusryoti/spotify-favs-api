17986509fd938a9dc927cc21146e1882250981d2
Markus Ryöti <markus.ryoti@student.tut.fi>
2023-06-11 13:23:01 +0300

    save spotify profile info as session data

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use base64::{engine::general_purpose, Engine as _};
 use hyper::{header::AUTHORIZATION, HeaderMap, StatusCode};
 
 extern crate redis;
+use redis::Commands;
 use redis::Connection;
-use redis::{Commands, ToRedisArgs};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
@@ -79,71 +79,137 @@ async fn create_session(
     if let Some(params) = params {
         let params: AccessTokenParams = params.0;
 
-        let url = "https://accounts.spotify.com/api/token";
+        let token_response = match get_tokens(&state, params).await {
+            Ok(response) => match get_token_body(response).await {
+                GetAccessTokenResponse::Ok(token_res) => token_res,
+                GetAccessTokenResponse::Err(e) => return GetAccessTokenResponse::Err(e),
+            },
+            Err(err) => return GetAccessTokenResponse::Err(err.to_string()),
+        };
 
-        let client_id = &state.client_id;
-        let client_secret = &state.client_secret;
+        // Get user information with the new access token and save profile data
+        let user_info = get_user_info(token_response.access_token.to_owned()).await;
 
-        let mut headers = HeaderMap::new();
-        headers.insert(
-            AUTHORIZATION,
-            get_hashed_header(&client_id, &client_secret)
-                .parse()
-                .unwrap(),
-        );
+        let user_info = match user_info {
+            SpotifyUserProfileResponse::Ok(info) => info,
+            SpotifyUserProfileResponse::Err(err) => return GetAccessTokenResponse::Err(err),
+        };
 
-        let redirect_uri = &state.redirect_uri;
+        let _session_id = save_session(&user_info, &state);
 
-        let mut form_params = HashMap::new();
-        form_params.insert("grant_type", "authorization_code");
-        form_params.insert("code", &params.code);
-        form_params.insert("redirect_uri", &redirect_uri);
-
-        let client = reqwest::Client::new();
-        let res = client
-            .post(url)
-            .headers(headers)
-            .form(&form_params)
-            .send()
-            .await;
-
-        match res {
-            Ok(response) => {
-                let body = match response.text().await {
-                    Ok(b) => b,
-                    Err(e) => return GetAccessTokenResponse::Err(e.to_string()),
-                };
-
-                let token_response: Result<SpotifyAccessTokenResponse, serde_json::Error> =
-                    serde_json::from_str(&body);
-
-                match token_response {
-                    Ok(res) => {
-                        let session_id = Uuid::new_v4();
-
-                        let serialized = json!(&res).to_string();
-
-                        let mut con = state.redis_conn.lock().unwrap();
-                        let _: () = con.set(session_id.to_string(), &serialized).unwrap();
-
-                        println!("session saved, key: {} data: {}", session_id, serialized);
-
-                        GetAccessTokenResponse::Ok(res)
-                    }
-                    Err(err) => GetAccessTokenResponse::Err(err.to_string()),
-                }
-            }
-            Err(err) => GetAccessTokenResponse::Err(err.to_string()),
-        }
+        GetAccessTokenResponse::Ok(token_response)
     } else {
         GetAccessTokenResponse::Err("invalid request".to_string())
     }
+}
+
+async fn get_tokens(
+    state: &AppState,
+    params: AccessTokenParams,
+) -> Result<reqwest::Response, reqwest::Error> {
+    let url = "https://accounts.spotify.com/api/token";
+
+    let client_id = &state.client_id;
+    let client_secret = &state.client_secret;
+
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        AUTHORIZATION,
+        get_hashed_header(&client_id, &client_secret)
+            .parse()
+            .unwrap(),
+    );
+
+    let redirect_uri = &state.redirect_uri;
+
+    let mut form_params = HashMap::new();
+    form_params.insert("grant_type", "authorization_code");
+    form_params.insert("code", &params.code);
+    form_params.insert("redirect_uri", &redirect_uri);
+
+    let client = reqwest::Client::new();
+
+    client
+        .post(url)
+        .headers(headers)
+        .form(&form_params)
+        .send()
+        .await
+}
+
+async fn get_token_body(response: reqwest::Response) -> GetAccessTokenResponse {
+    let body = match response.text().await {
+        Ok(b) => b,
+        Err(e) => return GetAccessTokenResponse::Err(e.to_string()),
+    };
+
+    let deserialized: Result<SpotifyAccessTokenResponse, serde_json::Error> =
+        serde_json::from_str(&body);
+
+    match deserialized {
+        Ok(res) => GetAccessTokenResponse::Ok(res),
+        Err(err) => GetAccessTokenResponse::Err(err.to_string()),
+    }
+}
+
+fn save_session(token_res: &SpotifyUserProfile, state: &AppState) -> Uuid {
+    let session_id = Uuid::new_v4();
+    let serialized = json!(token_res).to_string();
+
+    let mut con = state.redis_conn.lock().unwrap();
+    let _: () = con.set(session_id.to_string(), &serialized).unwrap();
+
+    println!("session saved, key: {} data: {}", session_id, serialized);
+
+    session_id
 }
 
 fn get_hashed_header(client_id: &str, client_secret: &str) -> String {
     let plain = format!("{}:{}", client_id, client_secret);
     let base64_encoded = general_purpose::STANDARD_NO_PAD.encode(plain.as_bytes());
     format!("Basic {}", base64_encoded)
+}
+
+async fn get_user_info(token: String) -> SpotifyUserProfileResponse {
+    let url = "https://api.spotify.com/v1/me";
+
+    let mut headers = HeaderMap::new();
+    headers.insert(AUTHORIZATION, format!("Bearer {}", token).parse().unwrap());
+
+    let client = reqwest::Client::new();
+
+    let result = client.get(url).headers(headers).send().await;
+
+    let body = match result {
+        Ok(res) => match res.text().await {
+            Ok(body) => body,
+            Err(err) => return SpotifyUserProfileResponse::Err(err.to_string()),
+        },
+        Err(err) => return SpotifyUserProfileResponse::Err(err.to_string()),
+    };
+
+    let parse_result: Result<SpotifyUserProfile, serde_json::Error> = serde_json::from_str(&body);
+
+    match parse_result {
+        Ok(profile) => {
+            println!("{:?}", profile);
+            SpotifyUserProfileResponse::Ok(profile)
+        }
+        Err(err) => SpotifyUserProfileResponse::Err(err.to_string()),
+    }
+}
+
+enum SpotifyUserProfileResponse {
+    Ok(SpotifyUserProfile),
+    Err(String),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SpotifyUserProfile {
+    country: String,
+    display_name: String,
+    email: String,
+    id: String,
 }
 
 #[derive(Clone)]
